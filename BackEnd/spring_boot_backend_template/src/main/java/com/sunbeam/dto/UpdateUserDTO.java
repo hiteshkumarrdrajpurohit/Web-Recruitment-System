3523d8a36f24a9484eb793dcff1b2dc89e329fbe
unnatidongare <unnatidongare@gmail.com>
2025-08-06 00:33:49 +0530

    modification in udateuserDTO and UserDTO

--- BackEnd/spring_boot_backend_template/src/main/java/com/sunbeam/dto/UpdateUserDTO.java
+++ BackEnd/spring_boot_backend_template/src/main/java/com/sunbeam/dto/UpdateUserDTO.java
@@ -8,30 +8,35 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UpdateUserDTO {
-	
-	private String phoneNumber;
-   
+
+    // Newly added fields to fix the error
+    private String firstName;
+
+    private String lastName;
+
+    private String password;
+
+    private String phoneNumber;
+
     private String skills;
-    
+
     private String address;
 
-	private  String city;
-	    
-	private String State;
- 
-	private Long ZipCode;
-	
-	private String Country;
+    private String city;
+
+    private String state;
+
+    private String country;
+
+    private Long zipCode;
 
     private String orgName;
 
-	private String Designation;
-	
+    private String designation;
+
     private LocalDate startDate;
-	  	   
+
     private LocalDate endDate;
-   
+
     private String summary;
-
-	}
-
+}
--- BackEnd/spring_boot_backend_template/src/main/java/com/sunbeam/dto/UserDTO.java
+++ BackEnd/spring_boot_backend_template/src/main/java/com/sunbeam/dto/UserDTO.java
@@ -1,20 +1,17 @@
 package com.sunbeam.dto;
 
-import java.time.LocalDate;
-
-import com.sunbeam.entity.types.UserRole;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 public class UserDTO {
-	  
-	  
-
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
 }
