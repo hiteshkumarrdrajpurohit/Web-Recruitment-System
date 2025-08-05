3c8cc2cf2582bcab16b57272a5c9181f23efe591
unnatidongare <unnatidongare@gmail.com>
2025-08-06 00:22:10 +0530

    modification in Application file

--- BackEnd/spring_boot_backend_template/src/main/java/com/sunbeam/Application.java
+++ BackEnd/spring_boot_backend_template/src/main/java/com/sunbeam/Application.java
@@ -6,7 +6,8 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 @SpringBootApplication // includes @Configuration
 public class Application {
 
@@ -37,5 +38,11 @@ public class Application {
 		return mapper;
 
 	}
+
+
+	@Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 }
